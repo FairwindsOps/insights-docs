bf5e799d87aebad3f85bca39946c437df2814570
Robert Brennan <bobby.brennan@gmail.com>
2020-03-10 15:52:56 +0000

    clean up config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  title: "Fairwinds Insights Documentation",
+  description: "Documentation for the Fairwinds Insights Kubernetes auditing platform",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+  ],
+  base: '/insights-docs',
   themeConfig: {
     docsRepo: 'FairwindsOps/insights-docs',
     editLinks: true,
-    editLinkText: 'Help us improve this page!',
-    logo: '/img/logo.svg',
+    editLinkText: 'Help us improve this page',
+    logo: 'img/logo.svg',
+    heroText: "",
     sidebar: [
       ['/', 'Home'],
       '/getting-started.html',
