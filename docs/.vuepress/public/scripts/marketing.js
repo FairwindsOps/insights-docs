5894807ac1ef188035c6b54c0a010069173a4f06
Robert Brennan <contact@rbren.io>
2021-01-15 18:43:57 +0000

    update docs from template

--- docs/.vuepress/public/scripts/marketing.js
+++ docs/.vuepress/public/scripts/marketing.js
@@ -21,3 +21,9 @@ s.parentNode.insertBefore(t,s)}(window, document,‘script’,
 ‘https://connect.facebook.net/en_US/fbevents.js’);
 fbq(‘init’, ‘159554595936922’);
 fbq(‘track’, ‘PageView’);
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
+new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
+’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,‘script’,‘dataLayer’,‘GTM-K5KK5H3’);
