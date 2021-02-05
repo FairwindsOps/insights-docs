24410c1f46035fab7011ed99453bc77aaa5358d7
Robert Brennan <contact@rbren.io>
2021-02-05 17:42:46 +0000

    change report name

--- docs/.vuepress/config-extras.js
+++ docs/.vuepress/config-extras.js
@@ -52,7 +52,7 @@ module.exports = {
         "/reports/polaris",
         "/reports/trivy",
         "/reports/goldilocks",
-        "/reports/prometheus-collector",
+        "/reports/resource-metrics",
         "/reports/opa",
         "/reports/kube-bench",
         "/reports/nova",
