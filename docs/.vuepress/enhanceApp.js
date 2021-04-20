6009c45101538d718541d69c6b20212675d2b7fd
Robert Brennan <contact@rbren.io>
2021-04-20 19:06:18 +0000

    add in redirects

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,29 @@
+export default ({ router }) => {
+  console.log('router');
+  router.addRoutes([
+    { path: '/reports/:target', redirect: to => {
+      return '/configure/reports/' + to.params.target;
+    }},
+    { path: '/integrations/:target', redirect: to => {
+      return '/configure/integrations/' + to.params.target;
+    }},
+    { path: '/architecture/architecture', redirect: to => {
+      return '/technical-details/architecture/architecture';
+    }},
+    { path: '/installation/insights-agent', redirect: to => {
+      return '/run/agent/installation';
+    }},
+    { path: '/features/rules', redirect: to => {
+      return '/configure/policy/rules';
+    }},
+    { path: '/features/continuous-integration', redirect: to => {
+      return '/run/ci/about';
+    }},
+    { path: '/features/workloads', redirect: to => {
+      return '/run/agent/workloads';
+    }},
+    { path: '/features/admission-controller', redirect: to => {
+      return '/run/admission/about';
+    }},
+  ])
+}
