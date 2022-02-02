0ca63a51524b24e32306d3cfe52ad39e8c0321dd
Robert Brennan <contact@rbren.io>
2022-02-02 16:35:37 +0000

    first pass at redirects

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -28,6 +28,57 @@ const redirects = [{
 }, {
   prefix: '/features/cli',
   redirect: '/configure/policy/cli',
+}, {
+  prefix: '/run/admission/configuration',
+  redirect: '/configure/admission/configuration',
+}, {
+  prefix: '/run/agent/configuration',
+  redirect: '/configure/agent/configuration',
+}, {
+  prefix: '/run/ci/configuration',
+  redirect: '/configure/ci/configuration',
+}, {
+  prefix: '/run/agent/report-hub',
+  redirect: '/configure/agent/install-hub',
+}, {
+  prefix: '/run/agent/about',
+  redirect: '/installation/agent/setup',
+}, {
+  prefix: '/run/ci/about',
+  redirect: '/installation/ci/about',
+}, {
+  prefix: '/run/ci/results',
+  redirect: '/installation/ci/results',
+}, {
+  prefix: '/run/ci/github',
+  redirect: '/installation/ci/github',
+}, {
+  prefix: '/run/admission/about',
+  redirect: '/installation/admission/about',
+}, {
+  prefix: '/run/admission/setup',
+  redirect: '/installation/admission/setup',
+}, {
+  prefix: '/configure/policy/rules',
+  redirect: '/configure/automation/integrations', // maybe pick a different one here
+}, {
+  prefix: '/configure/policy/cli',
+  redirect: '/configure/cli/cli',
+}, {
+  prefix: '/configure/integrations',
+  redirect: s => '/installation' + s.replace('/configure', ''),
+}, {
+  prefix: '/configure/reports',
+  redirect: s => '/technical-details' + s.replace('/configure', ''), // this isn't right yet
+}, {
+  prefix: '/run/agent',
+  redirect: s => '/code' + s.replace('/run/agent', '')
+}, {
+  prefix: '/run/admission',
+  redirect: s => '/code' + s.replace('/run/admission', '')
+}, {
+  prefix: '/technical-details/architecture/sample-reports',
+  redirect: '/code/sample-reports',
 }]
 
 export default ({ router }) => {
